9c19e1acd63e82b0a5759d5cd482c6aabef61cd2
John Doe <johndoe@example.com>
2025-03-27 20:02:07 +0200

    a

--- models.py
+++ models.py
@@ -10,3 +10,21 @@ class Book:
     def __str__(self):
         return f"<Book {self.name} - {self.inn}>"
 
+
+class Library:
+    def __init__(self, name: str):
+        self.name = name
+        self.list_book: list[Book] = []
+
+    def get_book(self, book_inn):
+        for book in self.list_book:
+            if book.inn == book_inn:
+                self.list_book.append(book)
+
+    def delete_book(self, book_inn):
+        for book in self.list_book:
+            if book.inn == book_inn:
+                self.list_book.remove(book)
+                break
+
+
